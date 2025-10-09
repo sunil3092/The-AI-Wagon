c207c029277dd63fad07769cd3b2bb2b45f09156
Sunil Gauda <sunilg3011992@gmail.com>
2025-10-09 19:39:48 +0100

    📝  Basic If and else

---
+++ python/3.Conditionals.py
@@ -0,0 +1,17 @@
+# if
+train_coming = True
+if train_coming:
+    print("Putting up the barriers!")
+
+# if else
+in_stock = True
+if in_stock:
+    print("Producing Invoice")
+else:
+    print("Please come back later")
+
+need_a_snack = input("Hey you need a snack ?")
+if need_a_snack =="yes" or "yep":
+    print(f"Lets grab some")
+else:
+    print(f"Okes")
